ab94d4e80cb00b9fda3fb139db632f6108109186
jgod1360 <--list>
2022-05-26 09:56:27 -0400

    Update

--- terraform/module/turbo/scripts/install_turbonomic.sh
+++ terraform/module/turbo/scripts/install_turbonomic.sh
@@ -34,10 +34,20 @@ ${ROLE_BINDING_FILE_CONTENT}
 EOF
 echo
 
+echo "Creating Turbonomic ConfigMap ..."
+kubectl create -f -<<EOF
+${CONFIGMAP_FILE_CONTENT}
+EOF
+echo
+
 echo "Installing Turbonomic Platform Operator"
 kubectl create -f -<<EOF
 ${OPERATOR_GROUP_FILE_CONTENT}
 EOF
+echo
+
+echo "Deploying Turbonomic Operator ..."
+kubectl create -f https://raw.githubusercontent.com/turbonomic/t8c-install/master/operator/deploy/operator.yaml -n "${TURBO_PROJECT}"
 echo
 
 echo "Creating Turbonomic Platform Subscription ..."
@@ -48,7 +58,7 @@ until oc get crd xls.charts.helm.k8s.io >> /dev/null 2>&1; do sleep 5; done
 echo
 
 echo "Creating Custom Resource Definition (xls.charts.helm.k8s.io) ..."
-kubectl create -f https://raw.githubusercontent.com/turbonomic/t8c-install/master/operator/deploy/crds/charts_v1alpha1_xl_crd.yaml -n "${TURBO_PROJECT}"
+kubectl apply -f https://raw.githubusercontent.com/turbonomic/t8c-install/master/operator/deploy/crds/charts_v1alpha1_xl_cr.yaml -n "${TURBO_PROJECT}"
 echo
 
 echo "Configuring the NGINX or the Ingress/Route ..."
@@ -57,22 +67,18 @@ ${NGINX_INGRESS_FILE_CONTENT}
 EOF
 echo
 
-echo "Creating Turbonomic ConfigMap ..."
-kubectl create -f -<<EOF
-${CONFIGMAP_FILE_CONTENT}
-EOF
-echo
+#echo "Deploying Turbonomic ..."
+#kubectl create -f -<<EOF
+#${KUBETURBO_DEPLOY_FILE_CONTENT}
+#EOF
+#echo
 
-echo "Deploying Turbonomic ..."
-kubectl create -f -<<EOF
-${KUBETURBO_DEPLOY_FILE_CONTENT}
-EOF
-echo
-
-
-echo "Creating Turbonomic Platform Deployment..."
-kubectl create -f https://raw.githubusercontent.com/turbonomic/t8c-install/master/operator/deploy/operator.yaml -n "${TURBO_PROJECT}"
-echo
 
 sleep 15
+echo "Listing the pods ..."
 kubectl get pods -n "${TURBO_PROJECT}"
+
+echo
+echo "#####################################################"
+echo "###### Installation Completed Successfully!!! #######"
+echo "#####################################################"
